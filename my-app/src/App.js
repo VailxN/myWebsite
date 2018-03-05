ade4e917f88f0e509337368615d5b74331e843ef
vailxn <limyueshing@gmail.com>
2018-03-05 18:44:03 +0800

    added component MenuItem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,34 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Menu } from 'semantic-ui-react'
 
 import home from './pages/home'
 import projects from './pages/projects'
 import about from './pages/about'
 
-import TopMenu from './components/TopMenu'
+import MenuItem from './components/MenuItem'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">        
-        <TopMenu />
+      <div className="App">  
+        <Menu pointing secondary>
+          <MenuItem to='/' name='home'/>
+          <MenuItem to='/about' name='about'/>
+          <MenuItem to='/projects' name='projects'/>
+        </Menu>      
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to My Website</h1>
         </header> */}
- 
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-        </ul>
         <hr />
 
         <Route exact path="/" component={home} />
